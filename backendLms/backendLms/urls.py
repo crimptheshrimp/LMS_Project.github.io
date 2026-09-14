fee7898368f43b85f4b5c7d2f660769461330f04
crimptheshrimp <christocat87@gmail.com>
2026-09-14 20:34:41 +0100

    Add backend health endpoint

--- backendLms/backendLms/urls.py
+++ backendLms/backendLms/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
+
+
 urlpatterns = [
+    path('', health_check, name='health-check'),
     path('admin/', admin.site.urls),
     path('api/', include('lms_app.urls')),
 ]
